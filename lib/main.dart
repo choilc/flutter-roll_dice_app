fd082b69f6c543dff3a8e3a9a8b738f14fc16be4
choilc <choilv99@gmail.com>
2023-07-25 17:12:48 +0700

    widget

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 93, 9, 230),
+        body: Center(
+          child: Text('Hellow world!'),
+        ),
+      ),
+    ),
+  );
+}
