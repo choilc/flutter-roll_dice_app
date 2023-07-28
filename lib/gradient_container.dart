23c7dfcc9d835531a07c7a13ac48b01f17f8a540
choilc <choilv99@gmail.com>
2023-07-28 17:26:13 +0700

    continue

---
+++ lib/gradient_container.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:roll_dice_app/styled_text.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.defaultBg({super.key})
+      : color1 = Colors.deepOrange,
+        color2 = Colors.blue;
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: Center(
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/dice-1.png',
+              width: 200,
+            ),
+            TextButton(
+              onPressed: () {
+                print('aa');
+              },
+              child: const Text('Roll Dice'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice_app/gradient_container.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 93, 9, 230),
-        body: Center(
-          child: Text('Hellow world!'),
-        ),
+        body: GradientContainer.defaultBg(),
       ),
     ),
   );
